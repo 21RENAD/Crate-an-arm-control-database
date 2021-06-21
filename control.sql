3d340982dec52be4da8e984b944e0dce3dc27d1f
21RENAD <85811010+21RENAD@users.noreply.github.com>
2021-06-21 17:18:51 +0300

    Add files via upload

---
+++ control.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 21 يونيو 2021 الساعة 16:14
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `control`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `al motor`
+--
+
+CREATE TABLE `al motor` (
+  `Motor1` int(3) NOT NULL,
+  `Motor2` int(3) NOT NULL,
+  `Motor3` int(3) NOT NULL,
+  `Motor4` int(3) NOT NULL,
+  `Motor5` int(3) NOT NULL,
+  `Motor6` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
